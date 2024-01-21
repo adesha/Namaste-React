57db4c65d9621c760493e81d80ef1c822daab218
adesha <adeshaholagodu@gmail.com>
2024-01-21 17:39:02 +0530

    calling swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,22 @@
 import RestaurantCard from "./RestaurantCard"
-import resList from "../utils/mockdata"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 const Body=()=>{
 
-    const [listOfRestaurants,setListOfRestaurants]=useState(resList);
+    const [listOfRestaurants,setListOfRestaurants]=useState([]);
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    const fetchData= async ()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0698281&lng=77.58960549999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+
+        const json = await data.json()
+        console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+
+        setListOfRestaurants(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    }
 
     //normal ks variable
     // let listOfRestaurants=[
